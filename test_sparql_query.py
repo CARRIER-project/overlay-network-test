e1555906ef4f202bf049e6a42e6db5bec2c89654
Djura Smits <djura.smits@gmail.com>
2021-01-27 14:00:17 +0100

    Fixed docker setup

---
+++ test_sparql_query.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+IMAGE = 'carrier-harbor2.carrier-mu.surf-hosted.nl/carrier/v6-carrier-py-sparql'
+
+HOST = 'http://vserver'
+PORT = 5000
+
+WAIT_TIME = 5
+
+
+
+def main():
+
+
+
+if __name__ == '__main__':
+    main()
